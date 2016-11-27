d9cc689737a6788ef87d479c9b52952bed9fc691
ZeroVitz <orlando112000@gmail.com>
2016-11-26 20:29:25 -0600

    Gripe realizado con FireBase usando Map.

--- app/src/main/java/com/example/greciaguzmn/natureheal/DetallesEnfermedad.java
+++ app/src/main/java/com/example/greciaguzmn/natureheal/DetallesEnfermedad.java
@@ -11,25 +11,38 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class DetallesEnfermedad extends AppCompatActivity {
+
 
     TextView txtNombre;
 
+    //Todos los Text del layout
     TextView txtTitulo;
+    TextView txtDescripcion;
+    TextView txtPrecaucion;
+    TextView txtRecomendacion;
+    TextView txtSintomas;
     //Mandamos a buscar la referencia a la raiz de nuestra BD no relacional
     private DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
     //Recorremos la BD hasta llegar al dato que nos interesa
-    private DatabaseReference mTitulo = mRootRef.child("enfermedades").child("gripe").child("nombre");
+    private DatabaseReference dbGripe = mRootRef.child("enfermedades").child("gripe");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalles_enfermedad);
-        txtNombre =(TextView)findViewById(R.id.dtituloenfermedad);
+        txtNombre = (TextView) findViewById(R.id.dtituloenfermedad);
         txtNombre.setText(getIntent().getStringExtra("nombreEnfermedad"));
-        //
-
+        // Todos los TextView del layout
         txtTitulo = (TextView) findViewById(R.id.dtituloenfermedad);
+        txtDescripcion = (TextView) findViewById(R.id.parrafoGripe);
+        txtPrecaucion = (TextView) findViewById(R.id.parrafo1Pre);
+        txtRecomendacion = (TextView) findViewById(R.id.parrafoRec);
+        txtSintomas = (TextView) findViewById(R.id.parrafoSintomas);
+
 
     }
 
@@ -38,13 +51,26 @@ public class DetallesEnfermedad extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        mTitulo.addValueEventListener(new ValueEventListener() {
+        dbGripe.addValueEventListener(new ValueEventListener() {
             //Este metodo sirve para cuando inicia y cuando sucede un cambio
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String data = dataSnapshot.getValue(String.class);
-                txtTitulo.setText(data);
+                //Nuestro respectivo Map que le hacemos un pequeño casting para soporte toda la info de gripe
+                Map <String, String> map = (Map)dataSnapshot.getValue();
+                //Le pedimos el dato con su clave especifica
+                String titulo = map.get("nombre");
+                String descripcion = map.get("descripcion");
+                String sintomas = map.get("sintomas");
+                String precaucion = map.get("precaucion");
+                String recomendacion = map.get("recomendacion");
+                //Agregamos los cambios
+                txtTitulo.setText(titulo);
+                txtDescripcion.setText(descripcion);
+                txtSintomas.setText(sintomas);
+                txtPrecaucion.setText(precaucion);
+                txtRecomendacion.setText(recomendacion);
             }
+
             //Este metodo sirve para cuando da error
             @Override
             public void onCancelled(DatabaseError databaseError) {
