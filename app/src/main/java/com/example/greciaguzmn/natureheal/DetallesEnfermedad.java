b500c5095b9b9bd9262992b956e738f9c63384c0
ZeroVitz <orlando112000@gmail.com>
2016-11-26 19:11:26 -0600

    Primer uso de FireBase con exito.
    Se logro que el titulo de Gripe se enlazara a la BD para actualizarse en cualquier momento

--- app/src/main/java/com/example/greciaguzmn/natureheal/DetallesEnfermedad.java
+++ app/src/main/java/com/example/greciaguzmn/natureheal/DetallesEnfermedad.java
@@ -5,9 +5,21 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class DetallesEnfermedad extends AppCompatActivity {
 
     TextView txtNombre;
+
+    TextView txtTitulo;
+    //Mandamos a buscar la referencia a la raiz de nuestra BD no relacional
+    private DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    //Recorremos la BD hasta llegar al dato que nos interesa
+    private DatabaseReference mTitulo = mRootRef.child("enfermedades").child("gripe").child("nombre");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +27,29 @@ public class DetallesEnfermedad extends AppCompatActivity {
         setContentView(R.layout.activity_detalles_enfermedad);
         txtNombre =(TextView)findViewById(R.id.dtituloenfermedad);
         txtNombre.setText(getIntent().getStringExtra("nombreEnfermedad"));
+        //
+
+        txtTitulo = (TextView) findViewById(R.id.dtituloenfermedad);
+
     }
 
-    
+    //Este es el metodo cuando se ejecuta la aplicacion.
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mTitulo.addValueEventListener(new ValueEventListener() {
+            //Este metodo sirve para cuando inicia y cuando sucede un cambio
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String data = dataSnapshot.getValue(String.class);
+                txtTitulo.setText(data);
+            }
+            //Este metodo sirve para cuando da error
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
