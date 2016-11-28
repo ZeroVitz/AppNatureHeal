5106d25f4cea4d8223ccd7c7ff45cd75596d164a
ZeroVitz <orlando112000@gmail.com>
2016-11-28 17:48:23 -0600

    Implementando FireBase a RecyclerView de Remedios pt 2

--- app/src/main/java/com/example/greciaguzmn/natureheal/Enfermedades.java
+++ app/src/main/java/com/example/greciaguzmn/natureheal/Enfermedades.java
@@ -2,7 +2,6 @@ package com.example.greciaguzmn.natureheal;
 
 import android.content.Context;
 import android.content.Intent;
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,7 +10,6 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
@@ -19,8 +17,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Enfermedades extends AppCompatActivity {
 
@@ -51,11 +47,12 @@ public class Enfermedades extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        FirebaseRecyclerAdapter<Enfermedad, EnfermedadViewHolder> FirebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Enfermedad, EnfermedadViewHolder>(
-           Enfermedad.class, R.layout.lista_view, EnfermedadViewHolder.class,mDatabase) {
+        FirebaseRecyclerAdapter<EnfermedadRemedio, EnfermedadViewHolder> FirebaseRecyclerAdapter = new FirebaseRecyclerAdapter<EnfermedadRemedio, EnfermedadViewHolder>(
+                EnfermedadRemedio.class, R.layout.lista_view, EnfermedadViewHolder.class,mDatabase) {
+
 
             @Override
-            protected void populateViewHolder(EnfermedadViewHolder viewHolder, Enfermedad model, final int position) {
+            protected void populateViewHolder(EnfermedadViewHolder viewHolder, EnfermedadRemedio model, final int position) {
 
                 final String post_key = model.getNombre();
 
--- app/src/main/java/com/example/greciaguzmn/natureheal/Remedios.java
+++ app/src/main/java/com/example/greciaguzmn/natureheal/Remedios.java
@@ -1,40 +1,118 @@
 package com.example.greciaguzmn.natureheal;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Remedios extends AppCompatActivity {
 
-    RecyclerView nlistcontainer2;
+    private RecyclerView nlistenfermedades;
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remedios);
-        nlistcontainer2 = (RecyclerView)findViewById(R.id.listcontainer2);
-        RemedioAdapter adapter = new RemedioAdapter(this, getDatasources2(), this);
-        nlistcontainer2.setLayoutManager(new LinearLayoutManager(this));
-        nlistcontainer2.setAdapter(adapter);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("remedios");
+
+        nlistenfermedades = (RecyclerView) findViewById(R.id.listaremedios);
+        nlistenfermedades.setHasFixedSize(true);
+        nlistenfermedades.setLayoutManager(new LinearLayoutManager(this));
 
     }
 
-    private List<ClaseRemedios> getDatasources2(){
-        List<ClaseRemedios> list = new ArrayList<>();
-        list.add(new ClaseRemedios("Té de lavanda"));
-        return list;
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
         getMenuInflater().inflate(R.menu.search_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<EnfermedadRemedio, Enfermedades.EnfermedadViewHolder> FirebaseRecyclerAdapter = new FirebaseRecyclerAdapter<EnfermedadRemedio, Enfermedades.EnfermedadViewHolder>(
+                EnfermedadRemedio.class, R.layout.lista_view2, Enfermedades.EnfermedadViewHolder.class,mDatabase) {
+
+
+            @Override
+            protected void populateViewHolder(Enfermedades.EnfermedadViewHolder viewHolder, EnfermedadRemedio model, final int position) {
+
+                final String post_key = model.getNombre();
+
+                viewHolder.setTitle(model.getNombre());
+                viewHolder.setImage(getApplicationContext(), model.getImagen());
+
+
+                viewHolder.mview.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if ((post_key.equals("Te de lavanda"))) {
+                            Intent intent = new Intent(getApplicationContext(), Detalle_remedio.class);
+                            startActivity(intent);
+                        }
+                    }
+                });
+
+            }
+        };
+
+        nlistenfermedades.setAdapter(FirebaseRecyclerAdapter);
+    }
+
+    public static class EnfermedadViewHolder extends RecyclerView.ViewHolder{
+
+        View mview;
+        TextView post_title;
+
+        public EnfermedadViewHolder(View itemView) {
+            super(itemView);
+
+            mview = itemView ;
+
+            post_title = (TextView) mview.findViewById(R.id.postTitle);
+
+            post_title.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+
+        public void setTitle(String title) {
+
+            TextView postTitle = (TextView) mview.findViewById(R.id.postTitle);
+            postTitle.setText(title);
+        }
+
+        public void setImage (Context ctx, String image) {
+
+            ImageView post_image = (ImageView) mview.findViewById(R.id.postImage);
+            Picasso.with(ctx).load(image).into(post_image);
+
+        }
+
+
     }
 
 }
